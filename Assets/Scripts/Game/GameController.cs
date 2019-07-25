b3bcfc645b2d141df9c0577b60d7a3babc189119
Chatchai Saratakij <CSaratakij@gmail.com>
2019-07-25 19:34:41 +0700

    Init project

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrainyChef
+{
+    public class GameController : MonoBehaviour
+    {
+        internal static GameController Instance = null;
+
+        internal Action OnGameStart;
+        internal Action OnGameOver;
+
+        internal bool IsStart { get; set; }
+
+        void Awake()
+        {
+            MakeSingleton();
+        }
+
+        void MakeSingleton()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        internal void GameStart()
+        {
+            if (IsStart)
+            {
+                return;
+            }
+
+            IsStart = true;
+            OnGameStart?.Invoke();
+        }
+
+        internal void GameOver()
+        {
+            if (!IsStart)
+            {
+                return;
+            }
+
+            IsStart = false;
+            OnGameOver?.Invoke();
+        }
+    }
+}
+
